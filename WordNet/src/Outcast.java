2ca8a556a9c34667c9ba2bc6ee8ffae130d6fbc4
shevchyk <illia.shevchyk@ukma.edu.ua>
2021-12-14 23:21:17 +0200

    WordNet structure

--- WordNet/src/Outcast.java
+++ WordNet/src/Outcast.java
@@ -9,12 +9,12 @@ public class Outcast {
     public String outcast(String[] nouns) {
         String outcast = "";
         int maxSum = -1;
-        for(int i = 0; i < nouns.length; i++) {
+        for (int i = 0; i < nouns.length; i++) {
             int currSum = 0;
-            for(String word: nouns) {
-                currSum+= wordNet.distance(nouns[i], word);
+            for (String word : nouns) {
+                currSum += wordNet.distance(nouns[i], word);
             }
-            if(currSum > maxSum) {
+            if (currSum > maxSum) {
                 maxSum = currSum;
                 outcast = nouns[i];
             }
